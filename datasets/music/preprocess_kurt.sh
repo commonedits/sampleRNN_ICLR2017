88d1a754bb2aea45e849b6b691283e157a1a30a5
Cortexelus <emperorcj@gmail.com>
2017-03-11 18:47:39 -0600

    kurt

--- datasets/music/preprocess_kurt.sh
+++ datasets/music/preprocess_kurt.sh
@@ -1,7 +1,7 @@
 # Next line with & executes in a forked shell in the background. That's parallel and not recommended.
 # Remove if causing problem
 #for file in "$DL_PATH"*_64kb.mp3; do ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" _64kb.mp3`.wav" & done 
-$DL_PATH=./kurt/
+DL_PATH="kurt/"
 for file in "$DL_PATH"*.mp3; do
 	ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" .mp3`.wav"
 done 
