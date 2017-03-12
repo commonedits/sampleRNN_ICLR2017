1b6f0af9ee0b317b2eaeb2db3df903121d5ba8b6
Cortexelus <emperorcj@gmail.com>
2017-03-11 19:06:32 -0600

    kurt

--- datasets/music/_2npy.py
+++ datasets/music/_2npy.py
@@ -23,7 +23,7 @@ def __fixed_shuffle(inp_list):
     # destructive operations; in place; no need to return
     raise ValueError("inp_list is neither a list nor a numpy.ndarray but a "+type(inp_list))
 
-data_path = os.path.abspath('./download/parts')
+data_path = os.path.abspath('./kurt/wav/parts')
 print data_path
 
 paths = sorted(glob.glob(data_path+"/*.flac"))
@@ -31,7 +31,7 @@ __fixed_shuffle(paths)
 
 arr = [(scikits.audiolab.flacread(p)[0]).astype('float16') for p in paths]
 np_arr = np.array(arr)
-np.save('all_music.npy', np_arr)
-np.save('music_train.npy', np_arr[:-2*256])
-np.save('music_valid.npy', np_arr[-2*256:-256])
-np.save('music_test.npy', np_arr[-256:])
+np.save('all_kurt.npy', np_arr)
+np.save('kurt_train.npy', np_arr[:-2*256])
+np.save('kurt_valid.npy', np_arr[-2*256:-256])
+np.save('kurt_test.npy', np_arr[-256:])
