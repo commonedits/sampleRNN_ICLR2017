a0aee0e8176545fe54b10c02a51d25ac4a070c93
Cortexelus <emperorcj@gmail.com>
2017-03-11 18:33:12 -0600

    process kurt

---
+++ datasets/music/preprocess_kurt.sh
@@ -0,0 +1,17 @@
+# Next line with & executes in a forked shell in the background. That's parallel and not recommended.
+# Remove if causing problem
+#for file in "$DL_PATH"*_64kb.mp3; do ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" _64kb.mp3`.wav" & done 
+$DL_PATH="kurt"
+for file in "$DL_PATH"*.mp3; do
+	ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" .mp3`.wav"
+done 
+echo "Cleaning up"
+rm "$DL_PATH"*.ogg
+
+echo "Preprocessing"
+python preprocess.py "$DL_PATH"
+echo "Done!"
+
+echo "Writing datasets"
+python _2npy.py
+echo "Done!"
