735590b792d57acb0a2f0185900beedb8f765aa9
Cortexelus <emperorcj@gmail.com>
2017-03-11 18:49:39 -0600

    kurt

--- datasets/music/preprocess_kurt.sh
+++ datasets/music/preprocess_kurt.sh
@@ -3,7 +3,8 @@
 #for file in "$DL_PATH"*_64kb.mp3; do ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" _64kb.mp3`.wav" & done 
 DL_PATH="kurt/"
 for file in "$DL_PATH"*.mp3; do
-	ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" .mp3`.wav"
+	echo $file
+	ffmpeg -i "$file" -ar 16000 -ac 1 "$file.wav"
 done 
 echo "Cleaning up"
 rm "$DL_PATH"*.ogg
