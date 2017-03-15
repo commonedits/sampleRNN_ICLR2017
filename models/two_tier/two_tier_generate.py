be04b84f0b35dabcd7592d896602774a40d20a8a
Cortexelus <emperorcj@gmail.com>
2017-03-15 06:12:47 -0500

    generater

--- models/two_tier/two_tier_generate.py
+++ models/two_tier/two_tier_generate.py
@@ -105,7 +105,7 @@ def get_args():
     parser.add_argument('--which_set', help='ONOM, BLIZZ, or MUSIC, or KURT',
             choices=['ONOM', 'BLIZZ', 'MUSIC', 'KURT'], required=True)
     parser.add_argument('--batch_size', help='size of mini-batch',
-            type=check_positive, choices=xrange(64,128), required=True)
+            type=check_positive, choices=xrange(0, 129), required=True)
 
     parser.add_argument('--debug', help='Debug mode', required=False, default=False, action='store_true')
     # NEW
